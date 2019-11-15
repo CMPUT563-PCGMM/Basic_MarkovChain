6c3e3dfe0565ab218a61e24b8756d4f172c5bfc6
Raymundo1 <cquptcxl@gmail.com>
2019-11-14 18:50:06 -0700

    add baseline randomModel

---
+++ RandomModel.py
@@ -0,0 +1,25 @@
+from BaseModel import BaseModel
+import random
+
+class RandomModel(BaseModel):
+
+    def __init__(self):
+        self.tile = ["F", "B", "M", "P", "O", "I", "S", "-"]
+
+    def learn(self, training_data, param_dict):
+        pass
+
+    def generate_new_room(self, initial_room_map, param_dict):
+        # print(initial_room_map)
+
+        empty_field = initial_room_map[2:-2, 2:-2]
+        # print(empty_field)
+
+        for i in range(empty_field.shape[0]):
+            for j in range(empty_field.shape[1]):
+                empty_field[i, j] = random.choice(self.tile)
+
+        return initial_room_map
+
+    def evaluate(self, evaluate_data, param_dict):
+        pass
